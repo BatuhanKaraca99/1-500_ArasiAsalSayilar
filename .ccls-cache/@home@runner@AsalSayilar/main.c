bbfbd87a9d9f58b6810e7da79fbf9f777a460c8a
Batuhan KARACA <47457138+BatuhanKaraca99@users.noreply.github.com>
2022-04-05 12:05:48 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@AsalSayilar/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+  short bulundu=0;
+  printf("1-500 sayı aralığında asal sayılar: ");
+  printf("2 3 5 7 11 13 17 19 23 "); //9 tane
+        for(int a=12;a<=500;a++)
+        {
+           int kackere=0;
+           if(a%2==0) kackere++;
+           if(a%3==0) kackere++; 
+           if(a%5==0) kackere++; 
+           if(a%7==0) kackere++;
+           if(a%11==0) kackere++;
+           if(a%13==0) kackere++;
+           if(a%17==0) kackere++;
+           if(a%19==0) kackere++;
+           if(a%23==0) kackere++; 
+           else if(kackere==0) {
+           printf("%d ",a); //86 tane
+           a++;
+           }
+        }
+        //95 tane
+}
